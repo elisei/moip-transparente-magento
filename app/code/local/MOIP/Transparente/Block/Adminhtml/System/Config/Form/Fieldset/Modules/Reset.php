b0347aca824d376a3fcd98bf8fb5a65026f03f7c
Bruno <brunoelisei@gmail.com>
2018-05-02 16:21:50 -0300

    Opção para limpeza de webhooks
    Melhorias na sessão "Informações da configuração atual" e inclusão de opção para apagar eventos.

--- app/code/local/MOIP/Transparente/Block/Adminhtml/System/Config/Form/Fieldset/Modules/Reset.php
+++ app/code/local/MOIP/Transparente/Block/Adminhtml/System/Config/Form/Fieldset/Modules/Reset.php
@@ -1,80 +1,142 @@
 <?php
 
-class MOIP_Transparente_Block_Adminhtml_System_Config_Form_Fieldset_Modules_Reset extends Mage_Adminhtml_Block_System_Config_Form_Fieldset {
-
+class MOIP_Transparente_Block_Adminhtml_System_Config_Form_Fieldset_Modules_Reset
+    extends Mage_Adminhtml_Block_Abstract implements Varien_Data_Form_Element_Renderer_Interface
+{
     /**
-     * Return header html for fieldset
+     * Render element html
      *
      * @param Varien_Data_Form_Element_Abstract $element
      * @return string
      */
-    protected function _getHeaderHtml($element)
-    {
-        $info_config = null;
-    	$validacao = Mage::getSingleton('transparente/standard')->getConfigData('validador_retorno');
-    	
-    	$code = Mage::getSingleton('adminhtml/config_data')->getStore();
-    	
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {   
+        $useContainerId = $element->getData('use_container_id');
+        $validacao = Mage::getSingleton('transparente/standard')->getConfigData('validador_retorno');
+        
+        $code = Mage::getSingleton('adminhtml/config_data')->getStore();
+        
+        $typeConsult = $element->getOriginalData('type_consult');
 
-    
+        if($typeConsult === 'db'){
+            $typeValueId = $element->getOriginalData('type_value_info');
+
+            $value = Mage::getSingleton('transparente/standard')->getConfigData($typeValueId);
+            
+            if(isset($value)){
+                return sprintf(
+                    '<tr class="moip-info-head" id="row_%s">
+                            <td colspan="5" >
+                                    <p class="moip-info-value">
+                                      <b>%s</b> -  %s
+                                    </p>
+                                    <p class="moip-info-note">
+                                       %s 
+                                       <hr>
+                                    </p>
+                            </td>
+                    </tr>',
+                        $element->getHtmlId(), 
+                        $element->getLabel(),
+                        $value,
+                        $element->getComment()
+                );
+            } else {
+                $textNotSet = $element->getOriginalData('text_not_set');
+                return sprintf(
+                    '<tr class="moip-info-head" id="row_%s">
+                            <td colspan="5" >
+                                    <h4 class="moip-info-title">%s</h4>
+                                    <p class="moip-info-note">
+                                        %s
+                                        <hr>
+                                    </p>
+                            </td>
+                    </tr>',
+                        $element->getHtmlId(),
+                        $element->getLabel(),
+                        $textNotSet
+                        
+                );
+            }
+        } else {
+            $path           = $element->getOriginalData('environment_path');
+            $environment    = $element->getOriginalData('type_environment');
+
+
+            $webhooks_set   = $this->getExternalConsult($path, $environment);
+            
+
+            $consult = json_decode($webhooks_set, true);
+
+            
+            
+            if(!empty($consult) && !isset($consult['ERROR']) ){
+                foreach ($consult as $key => $value) {
+                    $_html  .= '<tr class="moip-info-head">
+                                <td colspan="5" >
+                                        <h4 class="moip-info-title">'.$element->getLabel() .'</h4>
+                                       
+                                        <p class="moip-info-note"> Url: '.$value['target'].'</p>
+                                        <p class="moip-info-note"> Eventos de retorno: '.implode(" - ", $value['events']).'</p>
+                                         <p class="moip-info-note"> <a href="' .$this->urlClearWebhooks($value['id'], $environment).'">Apagar configuração de retorno</a></p>
+                                         <hr>
+                                </td>
+                            </tr>';
+                   
+                }
+                return $_html;
+            } else {
+                $textNotSet    = $element->getOriginalData('text_not_set');
+                $_html  = '<tr class="moip-info-head">
+                                <td colspan="5" >
+                                        <h4 class="moip-info-title">'.$element->getLabel().'</h4>
+                                        <p>'.$textNotSet.'</p>
+                                </td>
+                            </tr>';
+                return $_html;
+            }
+           
+
+           
             
            
-            $info_config .= '<hr><h3>Detalhes do token:</h3>';
+        }
 
-    		$oauth_dev = Mage::getSingleton('transparente/standard')->getConfigData('oauth_dev');
-            $publickey_dev= Mage::getSingleton('transparente/standard')->getConfigData('publickey_dev');
+        
+    }
+
+    public function getExternalConsult($path, $environment) {
+      
+        
+        $documento = 'Content-Type: application/json; charset=utf-8';
+        
+        if ($environment == "teste") {
+            $url = MOIP_Transparente_Model_Api::ENDPOINT_TEST.$path;
+            $header = "Authorization: OAuth " . Mage::getSingleton('transparente/standard')->getConfigData('oauth_dev');
             
-         
-    		
-            if(!is_null($oauth_dev)) {
+        } else {
+            $url = MOIP_Transparente_Model_Api::ENDPOINT_PROD.$path;
+            $header = "Authorization: OAuth " . Mage::getSingleton('transparente/standard')->getConfigData('oauth_prod');
+        }
 
-               $info_config .= '<tr id="row_moipall_advanced_rest">
-                                    <td class="label">
-                                        <label for="row_moipall_advanced_rest"> Seu Token Oauth - Ambiente de Teste:</label>
-                                    </td>
-                                    <td class="value">
-                                        <input readonly value="'.$oauth_dev.'" type="text" class="input-text" >
-                                    </td>
-                                </tr>';
-               
-              
-
-	    	} else {
-
-	    		
-                $info_config .= "<p>O Ambiente de Teste não está configurado</p>";
-
-	    	}
-
-
-    	
-    		
-    		$oauth_prod = Mage::getSingleton('transparente/standard')->getConfigData('oauth_prod');
-            $publickey_prod = Mage::getSingleton('transparente/standard')->getConfigData('publickey_prod');
-
-    		if(!is_null($oauth_prod)) {
-
-    			
-                $info_config .= '<tr id="row_moipall_advanced_rest">
-                                    <td class="label">
-                                        <label for="row_moipall_advanced_rest"> Seu Token Oauth - Ambiente de Produção:</label>
-                                    </td>
-                                    <td class="value">
-                                        <input readonly value="'.$oauth_prod.'" type="text" class="input-text" >
-                                    </td>
-                                </tr>';
-               
-
-	    	} else {
-
-	    		$info_config .= "<p>O Ambiente de Produção não está configurado</p>";
-
-	    	}
-    	   
+            $res = array();
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array($header, $documento));
+            curl_setopt($ch,CURLOPT_USERAGENT,'MoipMagento/2.0.0');
+            $res = curl_exec($ch);
+            curl_close($ch);
+          /*  var_dump($header); die();*/
+        return $res;
     
-        	$html = parent::_getHeaderHtml($element);
-        	$html = $html.$info_config;
-        return $html;
+    }
+
+    public function urlClearWebhooks($npr, $environment){
+         return Mage::helper("adminhtml")->getUrl("adminhtml/adminhtml_oauthmoip/KillWebhooks", array('_secure' => true))."id/".$npr."/environment/".$environment;
     }
 }
 ?>
--- app/code/local/MOIP/Transparente/controllers/Adminhtml/OauthmoipController.php
+++ app/code/local/MOIP/Transparente/controllers/Adminhtml/OauthmoipController.php
@@ -16,7 +16,46 @@ class MOIP_Transparente_Adminhtml_OauthmoipController extends  Mage_Adminhtml_Co
 			Mage::app()->getFrontController()->getResponse()->setRedirect($redirect_url);
 			return $this;
     }
+    public function KillWebhooksAction(){
+    	$data = $this->getRequest()->getParams();
+    	$documento = 'Content-Type: application/json; charset=utf-8';
+        
+        if ($data['environment'] == "teste") {
+            $url = MOIP_Transparente_Model_Api::ENDPOINT_TEST."preferences/notifications/".$data['id'];
+            $header = "Authorization: OAuth " . Mage::getSingleton('transparente/standard')->getConfigData('oauth_dev');
+            
+        } else {
+            $url = MOIP_Transparente_Model_Api::ENDPOINT_PROD."preferences/notifications/".$data['id'];
+            $header = "Authorization: OAuth " . Mage::getSingleton('transparente/standard')->getConfigData('oauth_prod');
+        }
 
+
+
+            $res = array();
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+            curl_setopt($ch, CURLOPT_NOBODY, true); 
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array($header, $documento));
+            curl_setopt($ch,CURLOPT_USERAGENT,'MoipMagento/2.0.0');
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $res = curl_exec($ch);
+            curl_close($ch);
+         
+
+			Mage::app()->cleanCache();
+			Mage::getSingleton('core/session')->addSuccess('Preferência apagada.');
+			$redirect_url = (Mage::helper('core/http')->getHttpReferer() ? Mage::helper('core/http')->getHttpReferer() : Mage::helper("adminhtml")->getUrl("*/system_config/edit/section/moipall/"));
+			
+			Mage::app()->getFrontController()->getResponse()->setRedirect($redirect_url);
+			
+
+
+        return $this;
+
+    }
     public function EnableWebhooksAction(){
 
 			$model = new Mage_Core_Model_Config();
@@ -125,6 +164,7 @@ class MOIP_Transparente_Adminhtml_OauthmoipController extends  Mage_Adminhtml_Co
 				$oauth_decode = json_decode($oauth, true);
 				$api->generateLog($oauth_decode['accessToken'], 'MOIP_Oauth.log');
 				$model->saveConfig('payment/moip_transparente_standard/oauth_dev', $oauth_decode['access_token'], 'default', 0);
+				$model->saveConfig('payment/moip_transparente_standard/mpa_dev', $oauth_decode['moipAccount']['id'], 'default', 0);
 				$public_key = $this->getKeyPublic($oauth_decode['access_token']);
 				$model->saveConfig('payment/moip_transparente_standard/publickey_dev', $public_key, 'default', 0);
 			} else {
@@ -133,6 +173,7 @@ class MOIP_Transparente_Adminhtml_OauthmoipController extends  Mage_Adminhtml_Co
 				$oauth_decode = json_decode($oauth, true);
 				$api->generateLog($oauth_decode['accessToken'], 'MOIP_Oauth.log');
 				$model->saveConfig('payment/moip_transparente_standard/oauth_prod', $oauth_decode['access_token'], 'default', 0);
+				$model->saveConfig('payment/moip_transparente_standard/mpa_prod', $oauth_decode['moipAccount']['id'], 'default', 0);
 				$public_key = $this->getKeyPublic($oauth_decode['access_token']);
 				
 				$model->saveConfig('payment/moip_transparente_standard/publickey_prod', $public_key, 'default', 0);
@@ -216,6 +257,8 @@ class MOIP_Transparente_Adminhtml_OauthmoipController extends  Mage_Adminhtml_Co
 		    $api->generateLog($public_key, 'MOIP_Oauth.log');
 		return $public_key;
 	}
+
+
 	public function getApi()
     {
         $api = Mage::getModel('transparente/api');
